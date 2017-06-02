dfc5e76c31be6fda5f90344d0a3eb08c02024a6b
Johann Horvat <johann@indiecam.com>
2017-06-02 11:31:39 +0200

    changed order inside autogen.sh due to: https://www.gnu.org/software/automake/manual/html_node/Error-required-file-ltmain_002esh-not-found.html

--- autogen.sh
+++ autogen.sh
@@ -7,12 +7,12 @@ OS=$(uname)
 if [ $OS == *"Darwin"* ]; then
 	LT="glibtoolize --copy"
 
-elif [ $OS == *"Linux"* ]; then
+elif [ $OS == "Linux" ]; then
 	LT="libtoolize --copy"
 fi
 
-echo "running aclocal..." && aclocal && \
 echo "running "$LT"..." && $LT && \
+echo "running aclocal..." && aclocal && \
 echo "running autoheader..." && autoheader && \
 echo "running automake..." && automake --add-missing && \
 echo "running autoreconf..." && autoreconf --force --install && \
